ceb51a1c573ed661f6abfdf6cfb1ac0b85f6b235
Nikolas Póda <nikolano@icloud.com>
2025-11-18 22:57:29 +0100

    Fix form page

---
+++ app/form/page.tsx
@@ -0,0 +1,13 @@
+export default function FormPage() {
+  return (
+    <main className="min-h-screen flex items-center justify-center text-white px-6 py-16">
+      <iframe
+        src="https://docs.google.com/forms/d/e/1FAIpQLSekzR8KaioBfKkkzLbY5CUVRUgb-vD8T9q2ftJo1HwhEhDx1Q/viewform?embedded=true"
+        width="100%"
+        height="4000"
+        className="rounded-2xl border border-white/10 shadow-xl"
+      >
+      </iframe>
+    </main>
+  );
+}
